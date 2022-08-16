d0941430fa28f4b0ac7aa4b3932c8b5dcdf6b84f
fukusuket <fukusuke.takahashi@gmail.com>
2022-08-16 21:28:09 +0900

    add rep macro.

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,35 +1,11 @@
-#include <bits/stdc++.h>]
+#include <bits/stdc++.h>
 using namespace std;
 
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
 int main() {
-   int N, K;
-   cin >> N >> K;
-   vector<int> A(N);
-   for (int i = 0; i < N; i++) {
-       cin >> A.at(i);
-   }
-
-   bool ans = false;
-
-    // すべての選び方を試して、総和がKになるものがあるかを調べる
-   for (int tmp = 0; tmp < (1 << 20); tmp++) {
-       bitset<20> s(tmp); // 最大20個なので20ビットのビット列として扱う
-
-       // ビット列の1のビットに対応する整数を選んだとみなして総和を求める
-       int sum = 0;
-       for (int i = 0; i < N; i++){
-           if (s.test(i)) {
-               sum += A.at(i);
-           }
-       }
-       if (sum == K) {
-           ans = true;
-       }
-   }
-
-   if (ans) {
-       cout << "YES" << endl;
-   } else {
-       cout << "NO" << endl;
-   }
+    rep(i, 3) {
+        cout << "Hello" << endl;
+        cout << "AtCoder" << endl;
+    }
 }
