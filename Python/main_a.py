b16488853442c6abaa5edfcaab6a93f7f3ff6dc7
fukusuket <41001169+fukusuket@users.noreply.github.com>
2022-09-10 21:04:07 +0900

    update template.

--- Python/main_a.py
+++ Python/main_a.py
@@ -5,6 +5,6 @@ def func(n):
 if __name__ == '__main__':
     # s = input()
     # n = int(input())
-    # a_s = [int(x) for x in input()]
+    # a_s = [int(x) for x in input().split()]
     # print(func(n))
     pass
